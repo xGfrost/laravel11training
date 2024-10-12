7ba66a2d5014d00e138dbb099e9cba237f15471c
xGfrost <fadelakbar528@gmail.com>
2024-10-12 20:51:37 +0700

    push 3

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{$title}}</x-slot:title>
-    <h3 class="text-xl">Welcome to my blog</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,50 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' =>[
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'ILAGOZ',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit facere magnam consequuntur itaque id tempora nam iure laborum deserunt voluptatibus, cum ducimus similique dignissimos minima adipisci hic soluta nulla consequatur.'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Jimmy',
+            'body' => ' sit amet consectetur adipisicing elit. Sit facere magnam consequuntur itaque id tempora nam iure Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit facere magnam consequuntur itaque id tempora nam iure laborum deserunt voluptatibus, cum ducimus similique dignissimos minima adipisci hic soluta nulla consequatur.'
+        ],
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'ILAGOZ',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit facere magnam consequuntur itaque id tempora nam iure laborum deserunt voluptatibus, cum ducimus similique dignissimos minima adipisci hic soluta nulla consequatur.'
+        ],
+
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Jimmy',
+            'body' => ' sit amet consectetur adipisicing elit. Sit facere magnam consequuntur itaque id tempora nam iure Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit facere magnam consequuntur itaque id tempora nam iure laborum deserunt voluptatibus, cum ducimus similique dignissimos minima adipisci hic soluta nulla consequatur.'
+        ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+
 });
 
 Route::get('/contact', function () {
